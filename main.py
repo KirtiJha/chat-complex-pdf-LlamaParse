facf7b8585bb8687d0890976f3700be75ddca76a
Kirti Jha <kirtijha@in.ibm.com>
2024-03-18 15:12:13 +0530

    added chatbot and vectorstore database

--- main.py
+++ main.py
@@ -3,17 +3,11 @@ import os
 import nest_asyncio
 
 nest_asyncio.apply()
-# API access to llama-cloud
-os.environ["LLAMA_CLOUD_API_KEY"] = "llx-i9NkjSBiUTwYAfo0G6UvrdAWp7ZkFJ84v0kaUUUWK6suTHvw"
-
-# Using OpenAI API for embeddings/llms
-os.environ["GEN_API_KEY"] = "pak-VcI5V05aaf42FbLqO7Fc6FR7KFSz2Gmap97DO1y3-I8"
 
 from genai import Client, Credentials
 from genai.extensions.langchain import LangChainEmbeddingsInterface
 from genai.schema import TextEmbeddingParameters
 from genai.extensions.langchain.chat_llm import LangChainChatInterface
-from genai.extensions.langchain import LangChainInterface
 from genai.schema import (
     DecodingMethod,
     ModerationHAP,
@@ -21,22 +15,20 @@ from genai.schema import (
     TextGenerationParameters,
     TextGenerationReturnOptions,
 )
-from llama_index.llms.langchain import LangChainLLM
-from llama_index.embeddings.langchain import LangchainEmbedding
 from llama_index.core import VectorStoreIndex
-from llama_index.core import Settings
 from llama_parse import LlamaParse
 from llama_index.core.retrievers import RecursiveRetriever
-from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
-from llama_index.core.query_engine import RetrieverQueryEngine
-
 from MarkdownElementNodeParser import MarkdownElementNodeParser
 
-credentials = Credentials(api_key=os.environ["GEN_API_KEY"],
-                          api_endpoint='https://bam-api.res.ibm.com/v2/text/chat?version=2024-01-10')
-client = Client(credentials=credentials)
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
 
-llm = LangChainChatInterface(
+def create_llm(gen_ai_key):
+    credentials = Credentials(api_key=gen_ai_key,
+                          api_endpoint='https://bam-api.res.ibm.com/v2/text/chat?version=2024-01-10')
+    client = Client(credentials=credentials)
+    llm = LangChainChatInterface(
     model_id="meta-llama/llama-2-70b-chat",
     client=client,
     parameters=TextGenerationParameters(
@@ -54,54 +46,55 @@ llm = LangChainChatInterface(
         hap=ModerationHAP(input=True, output=False, threshold=0.01)
     ),
 )
+    return llm
 
-credentials = Credentials(api_key=os.environ["GEN_API_KEY"])
-client = Client(credentials=credentials)
-embeddings = LangChainEmbeddingsInterface(
-    client=client,
-    model_id="sentence-transformers/all-minilm-l6-v2",
-    parameters=TextEmbeddingParameters(truncate_input_tokens=True),
-)
+def create_embeddings(gen_ai_key):
+    credentials = Credentials(api_key=gen_ai_key)
+    client = Client(credentials=credentials)
+    embeddings = LangChainEmbeddingsInterface(
+        client=client,
+        model_id="sentence-transformers/all-minilm-l6-v2",
+        parameters=TextEmbeddingParameters(truncate_input_tokens=True),
+    )
+    return embeddings
 
-llm = LangChainLLM(llm=llm)
-embed_model = LangchainEmbedding(embeddings)
+def parse_pdf(file_path, llama_cloud_api_key):
+    parser = LlamaParse(
+        api_key=llama_cloud_api_key,
+        result_type="markdown",
+        verbose=True,
+        language="en",
+        num_workers=2,
+    )
+    documents = parser.load_data(file_path)
+    return documents
 
-Settings.llm = llm
-Settings.embed_model = embeddings
+def build_recursive_index(documents):
+    node_parser = MarkdownElementNodeParser(llm=None, num_workers=8)
 
-documents = LlamaParse(result_type="markdown").load_data('./NVIDIA Earnings.pdf')
+    nodes = node_parser.get_nodes_from_documents(documents)
 
-node_parser = MarkdownElementNodeParser(llm=None, num_workers=8)
+    base_nodes, objects = node_parser.get_nodes_and_objects(nodes)
 
-nodes = node_parser.get_nodes_from_documents(documents)
+    base_nodes, node_mappings = node_parser.get_base_nodes_and_mappings(nodes)
 
-base_nodes, objects = node_parser.get_nodes_and_objects(nodes)
-index_with_obj = VectorStoreIndex(nodes=base_nodes + objects)
+    # chroma db as vector store
+    db = chromadb.PersistentClient(path="./chroma_db")
 
-raw_index = VectorStoreIndex.from_documents(documents)
+    chroma_collection = db.get_or_create_collection("my_collection")
 
-base_nodes, node_mappings = node_parser.get_base_nodes_and_mappings(nodes)
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-index = VectorStoreIndex(nodes=base_nodes)
-index_ret = index.as_retriever(top_k=15)
+    index = VectorStoreIndex(nodes=base_nodes, storage_context=storage_context)
+    vector_ret = index.as_retriever(similarity_top_k=1)
 
-recursive_index = RecursiveRetriever(
-    "vector",
-    retriever_dict={"vector": index_ret},
-    node_dict=node_mappings,
-    verbose=False,
-)
 
-reranker = FlagEmbeddingReranker(
-    top_n=5,
-    model="BAAI/bge-reranker-large",
-)
+    recursive_index = RecursiveRetriever(
+        "vector",
+        retriever_dict={"vector": vector_ret},
+        node_dict=node_mappings,
+        verbose=False,
+    )
 
-recursive_query_engine = RetrieverQueryEngine.from_args(recursive_index,
-                                                        node_postprocessors=[reranker], verbose=False)
-
-query = "What is the gross carrying amount of Total Amortizable Intangible Assets for Jan 29, 2023?"
-
-response = recursive_query_engine.query(query)
-
-print(response)
+    return recursive_index
